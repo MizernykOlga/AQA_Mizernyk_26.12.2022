ba2d6c3ed644104d9fd371797c68bcd7c5208b9f
Olga <mizernykolha@gmail.com>
2022-12-25 21:00:32 +0000

    added all files

---
+++ src/homework_2/MyApp.java
@@ -0,0 +1,16 @@
+package homework_2;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println();
+        double widthHomeTown = 50.3910500;
+        double lengthHomeTown = 24.2351400;
+
+        char degreeSign = '\u00B0';
+
+        System.out.println("Width of home town is " + widthHomeTown + degreeSign + ".");
+        System.out.println("Length of home town is " + lengthHomeTown + degreeSign + ".");
+
+
+    }
+}
