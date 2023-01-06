8921bc17904e583d20f7157ff05867f0112fcdb0
Olga <mizernykolha@gmail.com>
2023-01-06 21:31:31 +0000

    Homework5.1 - added while loop, once the user enter incorrect data type(text instead of int) will be asked to retry the input

--- src/homework6/CounterStrike.java
+++ src/homework6/CounterStrike.java
@@ -10,98 +10,142 @@ public class CounterStrike {
 
         Scanner scanner = new Scanner(System.in);
 
-        //as per lecture discussion don't do input verification for String
         System.out.println("1st team: ");
         teamName1 = scanner.nextLine();
 
-        //first team, each score stored in a var, verification on entering int data type
         int firstTeamPlayer1 = 0;
-        System.out.println("Enter player1 score: ");
-        if (scanner.hasNextInt()) {
-            firstTeamPlayer1 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-       System.out.println("Enter player2 score: ");
-        int firstTeamPlayer2 = 0;
-        if (scanner.hasNextInt()) {
-            firstTeamPlayer2 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player3 score: ");
-        int firstTeamPlayer3 = 0;
-        if (scanner.hasNextInt()) {
-            firstTeamPlayer3 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player4 score: ");
-        int firstTeamPlayer4 = 0;
-        if (scanner.hasNextInt()) {
-            firstTeamPlayer4 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player5 score: ");
-        int firstTeamPlayer5 = 0;
-        if (scanner.hasNextInt()) {
-            firstTeamPlayer5 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
+        while(true) {
+            System.out.println("Enter player1 score: ");
+            if (scanner.hasNextInt()) {
+                firstTeamPlayer1 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
         }
 
+        int firstTeamPlayer2 = 0;
+        while(true) {
+            System.out.println("Enter player2 score: ");
+            if (scanner.hasNextInt()) {
+                firstTeamPlayer2 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
+
+        int firstTeamPlayer3 = 0;
+        while (true) {
+            System.out.println("Enter player3 score: ");
+            if (scanner.hasNextInt()) {
+                firstTeamPlayer3 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
+
+        int firstTeamPlayer4 = 0;
+        while (true) {
+            System.out.println("Enter player4 score: ");
+            if (scanner.hasNextInt()) {
+                firstTeamPlayer4 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
+
+        int firstTeamPlayer5 = 0;
+        while (true) {
+            System.out.println("Enter player5 score: ");
+            if (scanner.hasNextInt()) {
+                firstTeamPlayer5 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
         //Second team, each score stored in a var, verification on entering int data type
-        scanner.nextLine();
+
         System.out.println("2nd team: ");
         teamName2 = scanner.nextLine();
 
-        System.out.println("Enter player1 score: ");
         int secondTeamPlayer1 = 0;
-        if (scanner.hasNextInt()) {
-            secondTeamPlayer1 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player2 score: ");
-        int secondTeamPlayer2 = 0;
-        if (scanner.hasNextInt()) {
-            secondTeamPlayer2 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player3 score: ");
-        int secondTeamPlayer3 = 0;
-        if (scanner.hasNextInt()) {
-            secondTeamPlayer3 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player4 score: ");
-        int secondTeamPlayer4 = 0;
-        if (scanner.hasNextInt()) {
-            secondTeamPlayer4 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
-        }
-        System.out.println("Enter player5 score: ");
-        int secondTeamPlayer5 = 0;
-        if (scanner.hasNextInt()) {
-            secondTeamPlayer5 = scanner.nextInt();
-        } else {
-            System.out.println("Oops, not a number. Try again!");
-            System.exit(0);
+        while (true) {
+            System.out.println("Enter player1 score: ");
+            if (scanner.hasNextInt()) {
+                secondTeamPlayer1 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
         }
 
+        int secondTeamPlayer2 = 0;
+        while (true) {
+            System.out.println("Enter player2 score: ");
+            if (scanner.hasNextInt()) {
+                secondTeamPlayer2 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
+
+        int secondTeamPlayer3 = 0;
+        while (true) {
+            System.out.println("Enter player3 score: ");
+            if (scanner.hasNextInt()) {
+                secondTeamPlayer3 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
+
+        int secondTeamPlayer4 = 0;
+        while (true) {
+            System.out.println("Enter player4 score: ");
+            if (scanner.hasNextInt()) {
+                secondTeamPlayer4 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
+
+        int secondTeamPlayer5 = 0;
+        while (true) {
+            System.out.println("Enter player5 score: ");
+            if (scanner.hasNextInt()) {
+                secondTeamPlayer5 = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Oops, not a number. Try again!");
+                scanner.nextLine();
+            }
+        }
         //closure, to avoid memory leak
         scanner.close();
 
